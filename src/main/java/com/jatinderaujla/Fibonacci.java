359238e177e906e5a3a627a731732e065d2e3cad
Jatinder Singh Aujla <jatinderaujla449@gmail.com>
2023-07-04 01:07:16 +0530

    dsa-practice initail commit with fibonacci series with different approaches

---
+++ src/main/java/com/jatinderaujla/Fibonacci.java
@@ -0,0 +1,72 @@
+package com.jatinderaujla;
+
+public class Fibonacci {
+
+    /**
+     * calculate fibonacci number of given number using Recursion
+     *
+     * @param num number who fibonacci need to be calculated
+     * @return fibonacci number
+     */
+    public int fibonacciWithRecursion(int num) {
+        if (num == 0) return 0;
+        if (num == 1) return 1;
+
+        return fibonacciWithRecursion(num - 1) + fibonacciWithRecursion(num - 2);
+    }
+
+    /**
+     * calculate fibonacci of given number using Dynamic Programming
+     *
+     * @param num given number
+     * @param arr arr of pre calculate fibonacci
+     * @return fibonacci number
+     */
+    public int fibonacciWithRecursionAndDynamicProgramming(int num, int[] arr) {
+        if (num == 0) return 0;
+        if (num == 1) return 1;
+        if (arr[num] != -1) {
+            return arr[num];
+        }
+        return arr[num] = fibonacciWithRecursionAndDynamicProgramming(num - 1, arr) + fibonacciWithRecursionAndDynamicProgramming(num - 2, arr);
+    }
+
+    /**
+     * calculate fibonacci of given number using Bottom UP approach(means Without Recursion)
+     * with Dynamic Programming
+     *
+     * @param num given number
+     * @return fibonacci number
+     */
+    public int fibonacciWithDynamicProgramming(int num) {
+        if (num == 0) return 0;
+        if (num == 1) return 1;
+        int[] arr = new int[num + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i <= num; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[num];
+    }
+
+    /**
+     * calculate fibonacci using 2 variables
+     *
+     * @param num given number
+     * @return fibonacci number
+     */
+    public int fibonacci(int num) {
+        int first = 0;
+        int second = 1;
+        int temp = -1;
+        if (num == 0) return 0;
+        if (num == 1) return 1;
+        for (int i = 2; i <= num; i++) {
+            temp = first + second;
+            first = second;
+            second = temp;
+        }
+        return temp;
+    }
+}
---
+++ src/test/java/com/jatinderaujla/FibonacciTest.java
@@ -0,0 +1,55 @@
+package com.jatinderaujla;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FibonacciTest {
+
+    private Fibonacci fibonacci;
+    private final int num = 6;
+    private final int expected = 8; //701408733 fib(44);
+
+    @BeforeEach
+    void setUp() {
+        this.fibonacci = new Fibonacci();
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.fibonacci = null;
+    }
+
+    @Test
+    void fibRecursionTest() {
+        int actual = fibonacci.fibonacciWithRecursion(num);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void fibRecursionDpTest() {
+        int[] arr = new int[this.num + 1];
+        Arrays.fill(arr, -1);
+        int actual = fibonacci.fibonacciWithRecursionAndDynamicProgramming(num, arr);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void fibDpTest() {
+        int actual = fibonacci.fibonacciWithDynamicProgramming(num);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void fibTest() {
+        int actual = fibonacci.fibonacci(num);
+        assertEquals(expected, actual);
+    }
+
+}
