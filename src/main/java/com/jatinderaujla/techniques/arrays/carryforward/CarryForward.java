42ae5715e704fef2f2bdcc953b23a699de87ae6b
Jatinder Singh Aujla <jatinderaujla449@gmail.com>
2024-07-16 14:11:07 +0530

    solved min length subarray with max and min values in it

--- src/main/java/com/jatinderaujla/techniques/arrays/carryforward/CarryForward.java
+++ src/main/java/com/jatinderaujla/techniques/arrays/carryforward/CarryForward.java
@@ -9,29 +9,39 @@ public class CarryForward {
     /**
      * Given a lowercase string count the no of a,g pairs exists in the given string
      * such that arr[i] < arr[j]
+     * Here traversing from right to left
+     *
      * @param str given string
      * @return no of A,G pair count
      */
-    public int countAGPairsReverseOrder(String str){
+    public int countAGPairsReverseOrder(String str) {
         int pairs = 0;
         int gCount = 0;
-        for (int i = str.length()-1; i >= 0; i--) {
-            if(str.charAt(i) == 'g'){
+        for (int i = str.length() - 1; i >= 0; i--) {
+            if (str.charAt(i) == 'g') {
                 gCount++;
-            } else if(str.charAt(i) == 'a'){
+            } else if (str.charAt(i) == 'a') {
                 pairs = pairs + gCount;
             }
         }
         return pairs;
     }
 
-    public int countAGPairs(String str){
+    /**
+     * Given a lowercase string count the no of a,g pairs exists in the given string
+     * such that arr[i] < arr[j]
+     * Here traversing from left to right
+     *
+     * @param str given string
+     * @return no of A,G pair count
+     */
+    public int countAGPairs(String str) {
         int pairs = 0;
         int aCount = 0;
         for (int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) == 'a'){
+            if (str.charAt(i) == 'a') {
                 aCount++;
-            } else if(str.charAt(i) == 'g'){
+            } else if (str.charAt(i) == 'g') {
                 pairs = pairs + aCount;
             }
         }
@@ -76,5 +86,73 @@ public class CarryForward {
         }
         Collections.reverse(leaders);
         return leaders.toArray(new Integer[0]);
+    }
+
+    /**
+     * Find the sub array with minimum length which start with or end with min and max elements in it
+     *
+     * @param arr given array
+     * @return length of smallest sub array
+     */
+    public int minSubArrayLengthReverseOrder(int[] arr) {
+        int max = arr[0];
+        int min = arr[0];
+        for (int j : arr) {
+            max = Math.max(max, j);
+            min = Math.min(min, j);
+        }
+
+        int minI = -1;
+        int maxI = -1;
+        int len = arr.length;
+        int minLen = arr.length;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] == max) {
+                maxI = i;
+                if(minI != -1){
+                    len = minI - maxI + 1;
+                }
+            } else if (arr[i] == min) {
+                minI = i;
+                if(maxI != -1){
+                    len = maxI - minI +  1;
+                }
+            }
+            if (len < minLen) {
+                minLen = len;
+            }
+        }
+        return minLen;
+    }
+
+    public int minSubArrayLength(int[] arr) {
+        int max = arr[0];
+        int min = arr[0];
+        for (int j : arr) {
+            max = Math.max(max, j);
+            min = Math.min(min, j);
+        }
+
+        int minI = -1;
+        int maxI = -1;
+        int len = arr.length;
+        int minLen = arr.length;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == max) {
+                maxI = i;
+                if(minI != -1){
+                    len = maxI - minI + 1;
+                }
+            } else if (arr[i] == min) {
+                minI = i;
+                if(maxI != -1){
+                    len = minI - maxI +  1;
+                }
+            }
+            if (len < minLen) {
+                minLen = len;
+            }
+        }
+        return minLen;
     }
 }
--- src/test/java/com/jatinderaujla/techniques/arrays/carryforward/CarryForwardTest.java
+++ src/test/java/com/jatinderaujla/techniques/arrays/carryforward/CarryForwardTest.java
@@ -34,7 +34,7 @@ class CarryForwardTest {
     }
 
     @Test
-    void testLeaderBruteForce() {
+    void leaderElementsBruteForce() {
         int[] arr = {3, 11, 5, 9, 6, 8};
         Integer[] expected = {11, 9, 8};
         Integer[] leaders = carryForward.leaderElementsBruteForce(arr);
@@ -42,10 +42,24 @@ class CarryForwardTest {
     }
 
     @Test
-    void testLeaderElements() {
+    void leaderElements() {
         int[] arr = {3, 11, 5, 9, 6, 8};
         Integer[] expected = {11, 9, 8};
         Integer[] leaders = carryForward.leaderElements(arr);
         assertArrayEquals(expected, leaders);
+    }
+
+    @Test
+    void minSubArrayLength() {
+        int[] arr = {9 ,1, 5, 9, 4, 2, 1};
+        int minLen = carryForward.minSubArrayLengthReverseOrder(arr);
+        assertEquals(2, minLen);
+    }
+
+    @Test
+    void minSubArrayLengthReverseOrder() {
+        int[] arr = {9 ,1, 5, 9, 4, 2, 1};
+        int minLen = carryForward.minSubArrayLength(arr);
+        assertEquals(2, minLen);
     }
 }
